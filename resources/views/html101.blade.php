9fd7f5648998c1571f7b71d167124b704b18dd0d
Peakop <thapakorn123th@gmail.com>
2025-12-01 17:13:46 +0700

    เซฟ
    เซฟ

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta chrsey="utf-8" />
+        <title>Workshop HTML</title>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+    </head>
+
+
+    <body>
+        <div class="container pt-5">
+            <h1>Workshop #HTML - FROM</h1>
+            <form>
+
+                <div class="row align-items-center">
+  <div class="col-4">
+    <label for="fName" class="col-form-label">
+        ชื่อ
+    </label>
+  </div>
+  <div class="col-6">
+    <input type="text" id="fName" class="form-control" 
+    
+   
+ class="col-6">
+    <span id="passwordHelpInline" class="form-text">
+    </span>
+  </div>
+
+  
+  <div class="col-4">
+    <label for="LName" class="col-form-label">
+        นามสกุล
+    </label>
+  </div>
+  <div class="col-6">
+    <input type="text" id="LName" class="form-control" 
+    
+
+ class="col-6">
+    <span id="passwordHelpInline" class="form-text">
+    </span>
+  </div>
+
+
+            </form>
+        </div> 
+    </body>
+</html>
